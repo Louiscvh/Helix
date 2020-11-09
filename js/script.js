77e3885bd6dacffe5389d9aaf4200b70c8d40353
Louis Cavecchi <cavecchilouis@gmail.com>
2020-11-09 22:44:15 +0100

    Fichier Json

--- js/script.js
+++ js/script.js
@@ -25,115 +25,115 @@ $(document).ready(function() {
   /* JSON configuration de Particles.js */
   particlesJS('particles-js',
   {
-  "particles": {
-    "number": {
-      "value": 123,
-      "density": {
-        "enable": true,
-        "value_area": 868.0624057955
-      }
-    },
-    "color": {
-      "value": "#acacac"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 1,
-        "color": "#ffffff"
-      },
-      "polygon": {
-        "nb_sides": 3
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.29987610382026364,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 0,
-      "color": "#ffffff",
-      "opacity": 0.2683101981549727,
-      "width": 5.681863019752363
-    },
-    "move": {
-      "enable": true,
-      "speed": 3.56,
-      "direction": "top",
-      "random": true,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 7102.328774690454,
-        "rotateY": 10000
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": false,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": false,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 0,
-        "line_linked": {
-          "opacity": 0
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
         }
       },
-      "bubble": {
-        "distance": 694.3260165176501,
-        "size": 105.57003759917487,
-        "duration": 2.7610625218245732,
-        "opacity": 8,
-        "speed": 3
+      "color": {
+        "value": "#ffffff"
       },
-      "repulse": {
-        "distance": 423.57642357642357,
-        "duration": 0.4
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
       },
-      "push": {
-        "particles_nb": 4
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
       },
-      "remove": {
-        "particles_nb": 2
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 1.8,
+        "direction": "top",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
       }
-    }
-  },
-  "retina_detect": true
-}
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
   );
 
 /* Gestion du curseur */
@@ -163,11 +163,9 @@ function togglePlay() {
 
 myAudio.onplaying = function() {
   isPlaying = true;
-  $('.wrapperPath path').css('stroke', 'rgba(255, 255, 255, 0.8')
 };
 myAudio.onpause = function() {
   isPlaying = false;
-  $('.wrapperPath path').css('stroke', 'rgba(255, 255, 255, 0.2)')
 };
 
 audioBtn.onclick = function(){
